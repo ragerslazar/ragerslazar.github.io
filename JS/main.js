51191a136341d2a82378fec6f18964ba8245edf8
Ragers Lazar <lazar.edu03@gmail.com>
2024-04-25 17:54:53 +0200

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-gh").addEventListener("click", function() {
+    window.open("https://github.com/ragerslazar", "_blank")
+});
+
+document.getElementById("btn-li").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/lazar-la2103/", "_blank")
+});
+
+document.getElementById("btn-cv").addEventListener("click", function() {
+    window.open("http://localhost/projets/Portfolio%20Dev%20FullStack/PDF/CV_Lazar_Lazarevic.pdf", "_blank")
+});
