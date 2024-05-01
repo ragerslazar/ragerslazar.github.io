d52ee724af3646a04fc64bf03ea8316380ea158a
Ragers Lazar <lazar.edu03@gmail.com>
2024-05-01 18:27:16 +0200

    Fix lien CV

--- JS/main.js
+++ JS/main.js
@@ -2,10 +2,23 @@ document.getElementById("btn-gh").addEventListener("click", function() {
     window.open("https://github.com/ragerslazar", "_blank")
 });
 
+document.getElementById("btn-gh-phone").addEventListener("click", function() {
+    window.open("https://github.com/ragerslazar", "_blank")
+});
+
 document.getElementById("btn-li").addEventListener("click", function() {
     window.open("https://www.linkedin.com/in/lazar-la2103/", "_blank")
 });
 
-document.getElementById("btn-cv").addEventListener("click", function() {
-    window.open("http://localhost/projets/portfolio_github/PDF/CV_Lazar_Lazarevic.pdf", "_blank")
+document.getElementById("btn-li-phone").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/lazar-la2103/", "_blank")
 });
+
+document.getElementById("btn-cv").addEventListener("click", function() {
+    window.open("https://github.com/ragerslazar/ragerslazar.github.io/blob/main/PDF/CV_Lazar_Lazarevic.pdf", "_blank")
+});
+
+document.getElementById("btn-cv-phone").addEventListener("click", function() {
+    window.open("https://github.com/ragerslazar/ragerslazar.github.io/blob/main/PDF/CV_Lazar_Lazarevic.pdf", "_blank")
+});
+
